530727d7968ffa79c8bff24bbb7d8f4c079324ca
JugadsinghReal <143512296+JugadsinghReal@users.noreply.github.com>
2024-11-01 19:51:22 +0100

    done

---
+++ main.js
@@ -0,0 +1,28 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+let email = document.getElementById('exampleInputEmail1')
+let text = document.getElementById('exampleFormControlTextarea1')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+
+if (alertTrigger) {
+   
+  alertTrigger.addEventListener('click', () => {
+    if (email.value == "" || text.value == ""){
+        appendAlert('Vul in', 'success')
+    } else {
+        appendAlert('Je bericht is gestuurd!', 'success')
+    }
+    
+  })
+}
